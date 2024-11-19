b224733c39015d1333f42d6e8b6f4881973159cf
Sahar Zolberg <161973653+SaharZo321@users.noreply.github.com>
2024-11-19 16:05:29 +0000

    fix algebranot working for negative numbers

--- Algebra.java
+++ Algebra.java
@@ -5,29 +5,37 @@
 
 public class Algebra {
 	public static void main(String args[]) {
-	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(36));
+		// Tests some of the operations
+		System.out.println(plus(-2, -3)); // 2 + 3
+		System.out.println(minus(-7, -2)); // 7 - 2
+		System.out.println(minus(2, -7)); // 2 - 7
+		System.out.println(times(3, -4)); // 3 * 4
+		System.out.println(plus(2, times(-4, 2))); // 2 + 4 * 2
+		System.out.println(pow(-5, 3)); // 5^3
+		System.out.println(pow(3, 5)); // 3^5
+		System.out.println(div(-12, -3)); // 12 / 3
+		System.out.println(div(5, 5)); // 5 / 5
+		System.out.println(div(25, 7)); // 25 / 7
+		System.out.println(mod(25, 7)); // 25 % 7
+		System.out.println(mod(-121, -6)); // 120 % 6
+		System.out.println(sqrt(36));
 		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(76123));
-	}  
+		System.out.println(sqrt(76123));
+	}
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
 		// Replace the following statement with your code
-		for (int i = 0; i < x2; i++) {
-			x1++;
+		int count = 0;
+		boolean isPlus = x2 > 0;
+		while (count != x2) {
+			if (isPlus) {
+				count++;
+				x1++;
+			} else {
+				count--;
+				x1--;
+			}
 		}
 		return x1;
 	}
@@ -35,20 +43,42 @@ public class Algebra {
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
 		// Replace the following statement with your code
-		for (int i = 0; i < x2; i++) {
-			x1--;
+		int count = 0;
+		boolean isPlus = x2 > 0;
+		while (count != x2) {
+			if (!isPlus) {
+				count--;
+				x1++;
+			} else {
+				count++;
+				x1--;
+			}
 		}
 		return x1;
 	}
 
+	public static boolean isSameSign(int x1, int x2) {
+		return x1 > 0 && x2 > 0 || x1 < 0 && x2 < 0;
+	}
+
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-        // Replace the following statement with your code
-		int base = x1;
-		for (int i = 0; i < x2 - 1; i++) {
-			x1 = plus(x1, base);
+		// Replace the following statement with your code
+		if (x1 == 0 || x2 == 0) {
+			return 0;
 		}
-		return x1;
+		int absX2 = x2 < 0 ? minus(0, x2) : x2;
+		int absX1 = x1 < 0 ? minus(0, x1) : x1;
+		int base = 0;
+		boolean isSameSign = isSameSign(x1, x2);
+		for (int i = 0; i < absX2; i++) {
+			if (isSameSign) {
+				base = plus(base, absX1);
+			} else {
+				base = minus(base, absX1);
+			}
+		}
+		return base;
 	}
 
 	// Returns x^n (for n >= 0)
@@ -61,29 +91,43 @@ public class Algebra {
 		return x;
 	}
 
-	// Returns the integer part of x1 / x2 
+	// Returns the integer part of x1 / x2
 	public static int div(int x1, int x2) {
-        // Replace the following statement with your code
-		int counter = 0;
-		while (x1 >= 0) {
-			x1 = minus(x1, x2);
+		// Replace the following statement with your code
+		int counter = -1;
+		int absX2 = x2 < 0 ? minus(0, x2) : x2;
+		int absX1 = x1 < 0 ? minus(0, x1) : x1;
+		boolean isSameSign = isSameSign(x1, x2);
+		while (absX1 >= 0) {
+
+			absX1 = minus(absX1, absX2);
 			counter++;
 		}
-		return counter - 1;
+		return isSameSign ? counter : times(counter, -1);
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-       return x1 - (times(x2, div(x1, x2)));
-	}	
+		int absX2 = x2 < 0 ? minus(0, x2) : x2;
+		if (x1 < 0) {
+			while (plus(x1, absX2) < absX2) {	
+				x1 = plus(x1, absX2);
+			}
+		} else {
+			while (x1 >= absX2) {	
+				x1 = minus(x1, absX2);
+			}
+		}
+		return x1;
+	}
 
-	// Returns the integer part of sqrt(x) 
+	// Returns the integer part of sqrt(x)
 	public static int sqrt(int x) {
-        for (int i = 1; i < x; i++) {
+		for (int i = 1; i < x; i++) {
 			if (pow(i, 2) > x) {
 				return i - 1;
 			}
 		}
 		return 0;
-	}	  	  
+	}
 }
